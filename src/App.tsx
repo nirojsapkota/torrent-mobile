b6243735302b25516e5d831164e063c152436d9c
Niroj Sapkota <nirojsapkota15@gmail.com>
2026-08-26 17:25:41 +1000

    Fix UI

--- src/App.tsx
+++ src/App.tsx
@@ -556,7 +556,7 @@ export default function App() {
             </div>
             <div>
               <h1 className="text-base font-bold text-white tracking-wide">OpenTorrent</h1>
-              <p className="text-[10px] text-gray-400 font-medium">BTT Protocol ©Niroj</p>
+              <p className="text-[10px] text-gray-400 font-medium">BTT Protocol Engine</p>
             </div>
           </div>
 
